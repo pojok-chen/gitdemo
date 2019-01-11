4d1bccc86f32cd2403c3d837f816e5b0446b5e18
chen <13934488655@163.com>
2019-01-11 16:26:34 +0800

    Initial commit

---
+++ gitdemo/src/com/cyz/core/demo/HelloDemo.java
@@ -0,0 +1,12 @@
+package com.cyz.core.demo;
+
+/**
+ * @author Chen
+ * @date 2019.1.11
+ * @version 1.0
+ */
+public class HelloDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello,world!");
+    }
+}
